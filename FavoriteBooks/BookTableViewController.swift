52e219b1383b097030b8099ea6e23dd81517be5f
Everett <brothers.everett@yahoo.com>
2023-10-13 09:23:57 -0600

    first commit

---
+++ FavoriteBooks/BookTableViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class BookTableViewController: UITableViewController {
+    
+    var books: [Book] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+
+        let book = books[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = book.title
+        content.secondaryText = book.description
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+    // MARK: - Navigation
+    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
+        guard let source = segue.source as? BookFormViewController,
+            let book = source.book else {return}
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            books.remove(at: indexPath.row)
+            books.insert(book, at: indexPath.row)
+            tableView.deselectRow(at: indexPath, animated: true)
+        } else {
+            books.append(book)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            books.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        if let vc = segue.destination as? BookFormViewController {
+            vc.book = books[indexPath.row]
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toForm", sender: tableView.cellForRow(at: indexPath))
+    }
+    
+}
